8c6dfff16801fd6c059b63124bfc1f498134fc9b
Richard Medina <richardmedina@richards-mbp.lan>
2021-03-10 07:45:55 +0800

    5th

--- src/App.js
+++ src/App.js
@@ -12,39 +12,39 @@ import './App.css';
 class App extends Component {
 
     componentDidMount() {
-        let styles = [
-            { src: '/portfolio/assets/css/lightbox.css' },
-            { src: '/portfolio/assets/css/owl.css' },
-            { src: '/portfolio/assets/css/templatemo-style.css' },
-            { src: '/portfolio/assets/css/fontawesome.css' },
-            { src: '/portfolio/vendor/bootstrap/css/bootstrap.min.css' },
-        ]
+        // let styles = [
+        //     { src: '/portfolio/assets/css/lightbox.css' },
+        //     { src: '/portfolio/assets/css/owl.css' },
+        //     { src: '/portfolio/assets/css/templatemo-style.css' },
+        //     { src: '/portfolio/assets/css/fontawesome.css' },
+        //     { src: '/portfolio/vendor/bootstrap/css/bootstrap.min.css' },
+        // ]
 
-        styles.map(item => {
-            const style = document.createElement('link')
-            const head = document.head
-            style.rel = 'stylesheet'
-            style.type = 'text/css'
-            style.href = item.src
-            head.insertBefore(style, head.childNodes[0]);
-            //document.head.appendChild();
-        })
+        // styles.map(item => {
+        //     const style = document.createElement('link')
+        //     const head = document.head
+        //     style.rel = 'stylesheet'
+        //     style.type = 'text/css'
+        //     style.href = item.src
+        //     head.insertBefore(style, head.childNodes[0]);
+        //     //document.head.appendChild();
+        // })
 
-        let scripts = [
-            { src: '/portfolio/vendor/bootstrap/js/bootstrap.bundle.min.js' },
-            { src: '/portfolio/assets/js/isotope.min.js' },
-            { src: '/portfolio/assets/js/owl-carousel.js' },
-            { src: '/portfolio/assets/js/lightbox.js' },
-            { src: '/portfolio/assets/js/custom.js' },
-        ]
+        // let scripts = [
+        //     { src: '/portfolio/vendor/bootstrap/js/bootstrap.bundle.min.js' },
+        //     { src: '/portfolio/assets/js/isotope.min.js' },
+        //     { src: '/portfolio/assets/js/owl-carousel.js' },
+        //     { src: '/portfolio/assets/js/lightbox.js' },
+        //     { src: '/portfolio/assets/js/custom.js' },
+        // ]
 
-        scripts.map(item => {
-            const script = document.createElement('script')
-            script.type = 'text/jsx'
-            script.src = item.src
-            script.async = SVGComponentTransferFunctionElement
-            document.body.appendChild(script)
-        });
+        // scripts.map(item => {
+        //     const script = document.createElement('script')
+        //     script.type = 'text/jsx'
+        //     script.src = item.src
+        //     script.async = SVGComponentTransferFunctionElement
+        //     document.body.appendChild(script)
+        // });
 
     }
 
